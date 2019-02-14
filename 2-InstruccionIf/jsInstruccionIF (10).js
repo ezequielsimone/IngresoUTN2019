eae3324f6bee6104f0614326ad161a994e7e1ffd
ezequielsimone <47385838+ezequielsimone@users.noreply.github.com>
2019-02-14 12:20:50 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,20 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	// math.round (  math.random() * (maximo - minimo) + minimo   );
+	// math.floor (  math.random() * ((maximo + 1) - minimo) + minimo  );
+	var aleatorio = Math.round (Math.random() * 9 + 1);
+
+if (aleatorio >= 9) {
+	alert("EXCELENTE " + aleatorio);
+}
+else if (aleatorio >= 4) {
+	alert("APROBÓ " + aleatorio);
+}
+else {
+	alert("Vamos, la proxima se puede " + aleatorio);
+}
+
+
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,7 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var aleatorio = Math.round (Math.random()* 9 + 1);
 
+	alert("El numero aleatorio es " + aleatorio)
 }//FIN DE LA FUNCIÓN
